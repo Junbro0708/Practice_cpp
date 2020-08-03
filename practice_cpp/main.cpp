af842f1f47d7476f347291fe9ad8fdcceb8c9a93
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-08-03 22:29:06 +0900

    Initial Commit

---
+++ practice_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice_cpp
+//
+//  Created by 박준형 on 2020/08/03.
+//  Copyright © 2020 박준형. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
