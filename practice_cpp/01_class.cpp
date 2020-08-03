847050ae10c10f301e6e24f7ee0e13f7f8e6681a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-08-03 23:37:18 +0900

    클래스부터 차근차근

---
+++ practice_cpp/01_class.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  practice_cpp
+//
+//  Created by 박준형 on 2020/08/03.
+//  Copyright © 2020 박준형. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+    double width = 1.2;
+    double height = 1.3;
+    
+public:
+    Rectangle(double x, double y):width(x), height(y) {
+        
+    }
+    Rectangle(double x):width(x){
+        
+    }
+    Rectangle() {
+        
+    }
+    void Print();
+};
+
+void Rectangle::Print() {
+    std::cout << "Width: " << width << ", Height: " << height << std::endl;
+}
+
+int main(int argc, const char * argv[]) {
+    Rectangle a {1.5, 1.6};
+    a.Print();
+    Rectangle b;
+    b.Print();
+}
